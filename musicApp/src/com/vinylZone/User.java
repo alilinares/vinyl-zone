b021461cdbfa27c10130f667b2cc53b84604ad13
Ali <therealalinares@gmail.com>
2022-11-04 15:55:48 -0400

    Add 5 parameter User constructor

--- musicApp/src/com/vinylZone/User.java
+++ musicApp/src/com/vinylZone/User.java
@@ -30,6 +30,15 @@ public class User {
 	 * @param bio
 	 */
     
+    public User(String username, String firstName, String lastName, String email, String password) {
+		super();
+		this.username = username;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
+    
 	public User(String username, String firstName, String lastName, String email, String password, String role,
 			File profilePhoto, String bio) {
 		super();
