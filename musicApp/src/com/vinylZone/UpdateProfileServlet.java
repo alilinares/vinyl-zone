b4a8422a050b1c7c709ac9b1d51679e70f068f99
Ali <therealalinares@gmail.com>
2022-11-25 22:55:46 -0500

    more committ

---
+++ musicApp/src/com/vinylZone/UpdateProfileServlet.java
@@ -0,0 +1,49 @@
+package com.vinylZone;
+
+import java.io.IOException;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class UpdateProfileServlet
+ */
+@WebServlet("/UpdateProfileServlet")
+public class UpdateProfileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Resource(name="jdbc/vinylZone") // Get Resource - JDBC Driver and other meta  (context.xml)
+	protected DataSource dataSource;
+	private ApplicationDao applicationDao;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateProfileServlet() {
+        super();
+		this.applicationDao = new ApplicationDao();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// send user to register.jsp
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("profile.jsp");
+		requestDispatcher.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
