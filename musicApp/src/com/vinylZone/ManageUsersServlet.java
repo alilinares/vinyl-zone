0c93a423b00afae703042f98c4e5747c41619f2a
Ali <therealalinares@gmail.com>
2022-11-25 22:55:39 -0500

    another committ

---
+++ musicApp/src/com/vinylZone/ManageUsersServlet.java
@@ -0,0 +1,53 @@
+package com.vinylZone;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class ManageUsersServlet
+ */
+@WebServlet("/ManageUsersServlet")
+public class ManageUsersServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private  ApplicationDao applicationDao;
+	
+	@Resource(name="jdbc/vinylZone") // Get Resource - JDBC Driver and other meta  (context.xml)
+	protected DataSource dataSource;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ManageUsersServlet() {
+        super();
+        this.applicationDao = new ApplicationDao();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//send to manageUsers.jsp
+		ArrayList<User> users = this.applicationDao.getListOfUsers(this.dataSource);
+		request.setAttribute("USER_LIST", users);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/manageUsers.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
